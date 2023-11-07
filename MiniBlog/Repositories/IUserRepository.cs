f921d215f953a982522f8845b591064916999b2d
Xinwei <xlu15@slb.com>
2023-11-07 20:18:28 +0800

    feat: add IUserRepostory and UserRepository

---
+++ MiniBlog/Repositories/IUserRepository.cs
@@ -0,0 +1,11 @@
+﻿using MiniBlog.Model;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace MiniBlog.Repositories;
+
+public interface IUserRepository
+{
+    public Task<User> FindUserByName(string name);
+    public Task<User> AddUser(User user);
+}
---
+++ MiniBlog/Repositories/UserRepository.cs
@@ -0,0 +1,28 @@
+﻿using MiniBlog.Model;
+using MongoDB.Driver;
+using System.Threading.Tasks;
+
+namespace MiniBlog.Repositories;
+
+public class UserRepository : IUserRepository
+{
+    private readonly IMongoCollection<User> userCollection;
+
+    public UserRepository(IMongoClient mongoClient)
+    {
+        var mongoDatabase = mongoClient.GetDatabase("MiniBlog");
+
+        userCollection = mongoDatabase.GetCollection<User>(User.CollectionName);
+    }
+
+    public async Task<User> AddUser(User newUser)
+    {
+        await userCollection.InsertOneAsync(newUser);
+        return await userCollection.Find(user => user.Name.Equals(newUser.Name)).FirstAsync();
+    }
+
+    public async Task<User> FindUserByName(string name)
+    {
+        return await userCollection.Find(user => user.Name.Equals(name)).FirstAsync();
+    }
+}
